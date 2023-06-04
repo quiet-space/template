aa367c8da355c491656d0a4706d5f9d06ae8d828
quiet-space <liexil97@gmail.com>
2023-06-04 19:41:14 +0900

    Revert "feat: router"
    This reverts commit 1fe3732b2649c7b1d1b6c06d6691f6a85d84722d.

--- template/src/layouts/MainLayout.vue
+++ template/src/layouts/MainLayout.vue
@@ -1,65 +1,116 @@
 <template>
-  <q-layout view="lHh Lpr lFf" class="items-center flex justify-center">
-    <div class="page-container">
-      <div class="header">
-  <!--        상단 메뉴-->
-        <div class="flex justify-between menu-container items-center">
-          <div>
-            {{ webTitle }}
-          </div>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
 
-          <div class="row q-gutter-x-md">
-            <q-btn v-for="menu of webMenus" flat size="sm" class="q-ma-none" @click="onClickMenu(menu)">
-              {{ menu.name }}
-            </q-btn>
-          </div>
-        </div>
-      </div>
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
 
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
 
-      <q-page-container class="content">
-          <router-view />
-      </q-page-container>
-    </div>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
   </q-layout>
 </template>
 
-<script setup>
-import {ref} from "vue";
-import {useRouter} from "vue-router";
+<script>
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
 
-const webTitle = ref('My Website')
-const webMenus = ref([{name: 'Notice', path: '/'}, {name: 'Board', path: 'Board'}, {name: 'Gallery', path: 'Gallery'}, {name: 'Visitor', path: 'Visitor'}])
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework',
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev',
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev',
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev',
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev',
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev',
+  },
+];
 
-const $router = useRouter()
-const onClickMenu = ({path}) => {
-  $router.push({path})
-}
+export default defineComponent({
+  name: 'MainLayout',
 
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
-
-<style scoped>
-.content {
-  width: 70vw;
-  height: 80vh;
-}
-
-.page-container {
-  border: 1px solid black;
-  border-radius: 10px;
-}
-
-.header {
-  border-bottom: 1px solid black;
-}
-
-.menu-container {
-  margin: 10px 15px 10px 15px;
-}
-
-.content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
--- template/src/pages/IndexPage.vue
+++ template/src/pages/IndexPage.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="flex flex-center">
-
-    This is Index Page
-
-  </div>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
 </template>
 
-<script setup>
+<script>
+import { defineComponent } from 'vue';
 
-
+export default defineComponent({
+  name: 'IndexPage',
+});
 </script>
